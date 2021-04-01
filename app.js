090c27f5294b40a886e63ba631e43f043e465578
nmparfait <mbamu.developer@gmail.com>
2021-04-01 14:07:52 -0400

    add the event listerner to the previous and next btn

--- app.js
+++ app.js
@@ -45,20 +45,40 @@ const job = document.getElementById('job')
 const info = document.getElementById('info')
 
 const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.nextBtn')
+const nextBtn = document.querySelector('.next-btn')
 const randomBtn = document.querySelector('.randomBtn')
 
 // set starting item
 let currentItem = 1
 
 //load initial item
-window.addEventListener('DOMContentLoaded', showPerson(currentItem))
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentItem)
+})
 
 //show person based on item
-function showPerson(person) {
-  const item = reviews[person]
+function showPerson() {
+  const item = reviews[currentItem]
   img.src = item.img
   author.textContent = item.name
   job.textContent = item.job
   info.textContent = item.text
 }
+
+// Next btn
+
+nextBtn.addEventListener('click', function () {
+  currentItem++
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0
+  }
+  showPerson()
+})
+
+prevBtn.addEventListener('click', function () {
+  currentItem--
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1
+  }
+  showPerson()
+})
